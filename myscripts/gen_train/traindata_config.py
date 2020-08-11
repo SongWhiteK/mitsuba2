5a521eb6fd5976dfe520209f150af20f6e79fedf
mine525 <mineyuki.t.aa@m.titech.ac.jp>
2020-08-11 16:34:31 +0900

    fix config

--- myscripts/gen_train/traindata_config.py
+++ myscripts/gen_train/traindata_config.py
@@ -7,11 +7,13 @@ Configuration of generating train data
 
 class TrainDataConfiguration:
     def __init__(self):
+        ############## EXECUTION MODES ##############
         self.DEBUG = True
         self.mode = "sample" # visualizing medium appearance
         self.medium_fix = False # generating fixed medium parameters
         self.scale_fix = False # do not scale shape objects
 
+        ############## FILE PATHS ##############
         if (self.mode is "visual"):
             self.XML_PATH = "C:\\Users\\mineg\\mitsuba2\\myscripts\\gen_train\\scene_templates\\visual_template.xml"
             self.spp = 64
@@ -29,17 +31,19 @@ class TrainDataConfiguration:
 
         self.SAMPLE_DIR = "C:\\Users\\mineg\\mitsuba2\\myscripts\\sample_files"
         # TODO: specify a file name dynamically
-        self.SAMPLE_OUT_NAME = "sample01.csv"
+        self.SAMPLE_OUT_NAME = "sample00.csv"
         self.TRAIN_DIR = "C:\\Users\\mineg\\mitsuba2\\myscripts\\train_paths"
         self.MAP_DIR = "C:\\Users\\mineg\\mitsuba2\\myscripts\\height_map"
         self.IMAGE_DIR = "C:\\Users\\mineg\\mitsuba2\\myscripts\\train_images"
 
-        self.itr = 5
+        ############## ITERATION NUMBERS ##############
+        self.itr = 10
         if(self.scale_fix):
             self.scene_batch_size = 1
         else:
             self.scene_batch_size = 1
 
+        ############## RANDOM NUMBER SEED ##############
         self.seed = 12
 
 
