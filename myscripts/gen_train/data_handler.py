be23ef3766652d40265a58bc6c804ee05c4fca64
mine525 <mineyuki.t.aa@m.titech.ac.jp>
2020-08-11 16:39:08 +0900

    Renewal data_handler.py
    Post processing sampled data and generate training image data
    It is just a prototype

--- myscripts/gen_train/data_handler.py
+++ myscripts/gen_train/data_handler.py
@@ -5,33 +5,48 @@ Processing trainingdata
 import glob
 import numpy as np
 import pandas as pd
-import json
+import cv2
+import utils
+
 
 class DataHandler:
-    def __init__(self, dir_path):
-        self.path = dir_path
-        self.update()
+    """
+    csv data handler for post proccessing
+    """
 
-    def update(self):
-        self.csv_list = glob.glob(self.path)
+    def __init__(self, config):
+        self.file_list = glob.glob(f"{config.SAMPLE_DIR}\\*")
+        self.train_image_dir_path = f"{config.IMAGE_DIR}"
+        self.map_dir_path = f"{config.MAP_DIR}"
+        self.train_sample_dir_path = f"{config.TRAIN_DIR}"
 
-    def csv_2_json(self, json_file):
+    def generate_train_data(self, offset=0):
         """
-        Integrate some csv files in one file, and convert to json format
-        Assume tags in csv files are wi_x, wi_y, wi_z, in_x, in_y, in_z, out_x, out_y, out_z
+        Generating height map w.r.t incident point and medium parameters
+        Args:
+            offset: ID offset
         """
-        for csv_name in self.csv_list:
-            n_sample = sum([1 for _ in open(csv_name)]) - 1
-            data = {"wi":np.zeros([1,3]), "in":np.zeros([1,3]), "out":np.zeros([1,3])}
-            sample = [data] * n_sample
+        height_map = None
+        id_data = 0
 
-            # get path data from csv files
-            with open(csv_name) as f:
-                reader = csv.DictReader(f)
-                for i, row in enumerate(reader):
-                    sample[i]["wi"] = row["wi"]
-                    sample[i]["in"] = row["in"]
-                    sample[i]["out"] = row["out"]
-            
-            # Integrate to one csv file
-            
+        # Process with given csv files
+        for i, file_name in enumerate(self.file_list):
+            # get id number and map number
+            data = pd.read_csv(file_name)
+            data["id"] = data.index + id_data + offset
+
+            # Generate training data with id
+            data.to_csv(f"{self.train_sample_dir_path}\\train_path{i:02}.csv", index=False)
+
+            # Get entire height map for a training data
+            height_map_path = glob.glob(f"{self.map_dir_path}\\height_map{i:02}.png")
+            height_map = cv2.imread(height_map_path[0])
+        
+            # Process with each training data 
+            for row in data.itertuples():
+                image = gen_train_image(row, height_map)
+
+                # Save height map image with id
+                cv2.imwrite(f"{self.image_dir_path}\\train_image{row.id:06}.png", image)
+                id_data += 1
+
