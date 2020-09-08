5a3889d2e74c3def7ac3ba126720da5d02a9c1c2
Mine <mine.go.0525@gmail.com>
2020-09-08 15:47:00 +0900

    Minor fix render.py

--- myscripts/render/render.py
+++ myscripts/render/render.py
@@ -1,15 +1,13 @@
 import os
+import time
 import mitsuba
-import enoki as ek
 import integrate
 import render_config as config
 
 mitsuba.set_variant(config.variant)
 
-from mitsuba.core import Float, UInt32, UInt64, Vector2f, Vector3f
-from mitsuba.core import Bitmap, Struct, Thread
+from mitsuba.core import Thread
 from mitsuba.core.xml import load_file
-from mitsuba.render import ImageBlock
 
 ##### Setting scene #####
 # Scene path
@@ -22,7 +20,13 @@ Thread.thread().file_resolver().append(os.path.dirname(scenepath))
 scene = load_file(scenepath)
 
 # Rendering settings
-spp = 64
+spp = 2048
 sample_per_pass = 64
 
+print("Rendering start")
+
+start = time.time()
 integrate.render(scene, spp, sample_per_pass)
+process_time = time.time() - start
+
+print(f"Rendering end (took {process_time}s)")
