b201e20c64b4129dcff3b6da7e59e786576ea02a
Mine <mine.go.0525@gmail.com>
2020-11-04 12:12:49 +0900

    Fix for multiprocessing

--- myscripts/render/render.py
+++ myscripts/render/render.py
@@ -4,6 +4,7 @@ import mitsuba
 import integrate
 import render_config as config
 import data_pipeline
+from multiprocessing import freeze_support
 
 sys.path.append("myscripts/render/dict")
 from scene_dict import scene_dict
@@ -14,23 +15,25 @@ mitsuba.set_variant(config.variant)
 from mitsuba.core import Thread
 from mitsuba.core.xml import load_file, load_dict
 
-##### Setting scene #####
-bdata = data_pipeline.BSSRDF_Data()
-meshes_cube().register_params(bdata)
+if __name__ == "__main__":
+    freeze_support()
+    ##### Setting scene #####
+    bdata = data_pipeline.BSSRDF_Data()
+    meshes_cube().register_params(bdata)
 
-meshes_cube().register_all_mesh(bdata)
+    meshes_cube().register_all_mesh(bdata)
 
-scene_dict = bdata.add_object(scene_dict)
-scene = load_dict(scene_dict)
+    scene_dict = bdata.add_object(scene_dict)
+    scene = load_dict(scene_dict)
 
-# Rendering settings
-spp = config.spp
-sample_per_pass = config.sample_per_pass
+    # Rendering settings
+    spp = config.spp
+    sample_per_pass = config.sample_per_pass
 
-print("Rendering start")
+    print("Rendering start")
 
-start = time.time()
-integrate.render(scene, spp, sample_per_pass, bdata)
-process_time = time.time() - start
+    start = time.time()
+    integrate.render(scene, spp, sample_per_pass, bdata)
+    process_time = time.time() - start
 
-print(f"Rendering end (took {process_time}s)")
+    print(f"Rendering end (took {process_time}s)")
