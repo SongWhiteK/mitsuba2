c82a8254550c32f0cbeb00801ee115899ec27a33
Mine <mine.go.0525@gmail.com>
2020-11-12 17:32:50 +0900

    Implement resampling outgoing direction

--- myscripts/render/utils_render.py
+++ myscripts/render/utils_render.py
@@ -10,6 +10,8 @@ import torch
 
 mitsuba.set_variant("gpu_rgb")
 
+from mitsuba.core import warp
+
 def index_spectrum(spec, idx):
     m = spec[0]
 
@@ -17,5 +19,13 @@ def index_spectrum(spec, idx):
     m[ek.eq(idx, 2)] = spec[2]
 
     return m
+
+
+def resample_wo(si, sampler, active):
+    d_out_local = warp.square_to_cosine_hemisphere(sampler.next_2d(active))
+    d_out_pdf = warp.square_to_cosine_hemisphere_pdf(d_out_local)
+    d_out = si.to_world(d_out_local)
+
+    return d_out, d_out_pdf
 
 
