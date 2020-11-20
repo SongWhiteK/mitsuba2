0c3c32983cab21cc1601584dad0054938539b6cb
Mine <mine.go.0525@gmail.com>
2020-11-20 14:49:30 +0900

    Change rendering configuration
    For rendering with VAE BSSRDF

--- myscripts/render/render_config.py
+++ myscripts/render/render_config.py
@@ -1,19 +1,22 @@
 """Configuration for rendering with path tracing and VAE BSSRDF"""
 
+from os import truncate
+
+
 variant = "gpu_rgb"
 rr_depth = 5
-max_depth = 3
-spp = 1
-sample_per_pass = 1
+max_depth = 10
+spp = 32
+sample_per_pass = 2048
 
-film_width = 4
-film_height = 4
+film_width = 256
+film_height = 256
 
 seed = 4
 
 model_name = "best_model"
 
 enable_bssrdf = True
-visualize_invalid_sample = True
+visualize_invalid_sample = False
 multi_process = False
 
