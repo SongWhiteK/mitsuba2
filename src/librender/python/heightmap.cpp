b73e31694d070b755546f885ad6a4b36f103ad3a
Mine <mine.go.0525@gmail.com>
2020-12-16 19:52:27 +0900

    Implement clip_map()

--- src/librender/python/heightmap.cpp
+++ src/librender/python/heightmap.cpp
@@ -39,15 +39,27 @@ public:
         std::cout << "shape: " << result.size() << ", " << m_shape_result[0] << ", "
                   << m_shape_result[1] << ", " << m_shape_result[2] << std::endl;
 
-        for(int i = 0; i < n_sample; i++){
-            for(int j = 0; j < m_im_size; j++){
-                for(int k = 0; k < m_im_size; k++){
+        // Roop of sampling for each ray
+        for (int i = 0; i < n_sample; i++){
+            int32_t id_i = *mesh_id.data(i);
+            if(id_i == 0) continue;
 
-                }
-            }
+            result[i] = clip_map(i, i+1);
         }
 
         return result;
+    }
+
+    array_i clip_map(int map_id, int32_t num){
+        array_i cliped_map{m_shape_result};
+
+        for (int i = 0; i < m_im_size; i++){
+            for(int j = 0; j < m_im_size; j++){
+                *cliped_map.mutable_data(0, i, j) = num;
+            }
+        }
+
+        return cliped_map;
     }
 
     
