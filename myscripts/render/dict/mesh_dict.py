79ca4c4733f08cffbf69d61983bfc11b8602e4e8
Mine <mine.go.0525@gmail.com>
2021-01-28 15:36:09 +0900

    Minor fix

--- myscripts/render/dict/mesh_dict.py
+++ myscripts/render/dict/mesh_dict.py
@@ -105,5 +105,9 @@ class meshes_leather(meshes_cube):
         self.height_max[5] = 1
         self.filename[5] = f"C:/Users/mineg/mitsuba2/myscripts/render/dict/serialized/leather_top{leather_num:02}.serialized"
 
-        map_path = f"C:/Users/mineg/mitsuba2/myscripts/train_data/height_map/height_map{leather_num:02}.png"
+        if(leather_num != 6):
+            map_path = f"C:/Users/mineg/mitsuba2/myscripts/train_data/height_map/height_map{leather_num:02}.png"
+        else:
+            map_path = f"C:/Users/mineg/mitsuba2/myscripts/train_data/height_map_test/height_map{leather_num:02}.png"
+
         self.map[5] = np.array(Image.open(map_path))
--- myscripts/render/render.py
+++ myscripts/render/render.py
@@ -21,9 +21,9 @@ if __name__ == "__main__":
         
     ##### Setting scene #####
     bdata = data_pipeline.BSSRDF_Data()
-    mesh = meshes_cube(1)
+    mesh = meshes_leather(1, 6)
 
-    mesh.register_params(bdata, ior=1.5, scale=1.0, sigma_t = 1.0, albedo = 0.9, g = 0.0)
+    mesh.register_params(bdata, ior=1.5, scale=1.0, sigma_t = 1.0, albedo = 0.9, g = 0.5)
 
     mesh.register_all_mesh(bdata)
 
